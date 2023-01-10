ce8990fc841d956445cb0e464b3a3952775efa64
michaelprete21 <106406676+michaelprete21@users.noreply.github.com>
2023-01-10 16:12:53 -0700

    updated text file to JSON file. Added search functionality. Added error TRY/EXCEPT/ELSE statements.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from random import choice, randint, shuffle
 from tkinter import *
 from tkinter import messagebox # must be imported seperately as it's just a module of code, not a class.
 import pyperclip
+import json
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -34,6 +35,12 @@ def save():
     website = website_input.get()
     username = username_input.get()
     password = password_input.get()
+    new_data = {
+        website:
+            {"email" : username,
+            "password" : password,
+             }
+    }
 
     if len(website) == 0 or len(username) == 0 or len(password) == 0:
         messagebox.showinfo(title="Error Notification: ", message="Please do not leave any fields empty. ")
@@ -43,12 +50,39 @@ def save():
                                                               f" {username} "
                                                               f"\nPasswor"
                                                       f"d: {password} \n\nIs it ok to save?")
-
         if is_ok:
-            with open("data_password.txt", "a") as dp:
-                dp.write(f"\n{website} || {username} || {password}\n")
-            website_input.delete(0, 'end')
-            password_input.delete(0, 'end')
+            try:
+                with open("data.json", "r") as dp:
+                    # Read old data
+                    data = json.load(dp)
+                    # Updating old data with new data
+                    data.update(new_data)
+            except FileNotFoundError: # If json file is empty or does not exist.
+                with open("data.json", "w") as dp:
+                    json.dump(new_data, dp, indent=4)
+            else:
+                with open("data.json", "w") as dp:
+                    json.dump(data, dp, indent=4)
+            finally:
+                website_input.delete(0, 'end')
+                password_input.delete(0, 'end')
+
+# ---------------------------- SEARCH ------------------------------- #
+
+def find_password():
+    website = website_input.get().title()
+    try:
+        with open("data.json", "r") as dp:
+            pass_data = json.load(dp)
+            try:
+                messagebox.showinfo(f"Website: {website}\n\n", message=f"Email/Username: "
+                                                                  f"{pass_data[website]['email']}\n"
+                                                                  f"Password: {pass_data[website]['password']}")
+            except KeyError:
+                messagebox.showinfo(title="Error", message=f"No details for the website or program exist. ")
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message=f"No Data File Found.")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -63,9 +97,9 @@ canvas.create_image(100, 100, image=lock_img)
 canvas.grid(column=1, row=0)
 
 # Website labels & entry
-website_label = Label(text="Website:")
+website_label = Label(text="Website/Program:")
 website_label.grid(row=1, column=0)
-website_input = Entry(width=35)
+website_input = Entry(width=21)
 website_input.grid(column=1, row=1, columnspan=2, sticky="EW")
 website_input.focus()
 
@@ -87,9 +121,11 @@ password_input.grid(column=1, row=3, columnspan=2, sticky="EW")
 
 # Buttons
 generate_password_b = Button(text="Generate Password", command=password_fill)
-generate_password_b.grid(column=2, row=3)
+generate_password_b.grid(column=2, row=3, sticky="EW")
 submit_info = Button(text="Add", width=36, command=save)
 submit_info.grid(column=1, row=4, columnspan=2, stick="EW")
+search_b = Button(text="Search", width=15, command=find_password)
+search_b.grid(column=2, row=1, sticky="EW")
 
 
 window.mainloop()
